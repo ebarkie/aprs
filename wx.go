9131fd3fe90852cd85d900eb77500db3b69c41b1
Eric Barkie <ebarkie@users.noreply.github.com>
2017-08-28 21:31:38 -0400

    Rewrite weather text generator
    I think this is simpler...

--- wx.go
+++ wx.go
@@ -10,218 +10,157 @@ import (
 	"time"
 )
 
-// Wx holds the weather station information.
+// Wx represents a weather station observation.
 type Wx struct {
 	Lat  float64
 	Lon  float64
 	Type string
-	o    obs
+
+	Timestamp time.Time
+
+	Altimeter       float64
+	Humidity        int
+	RainRate        float64
+	RainLast24Hours float64
+	RainToday       float64
+	SolarRad        int
+	Temp            int
+	WindDir         int
+	WindGust        int
+	WindSpeed       int
 }
 
-// obs represents an observation to be encoded.
-type obs struct {
-	altimeter       *float64
-	humidity        *int
-	rainRate        *float64
-	rainLast24Hours *float64
-	rainToday       *float64
-	solarRadiation  *int
-	temperature     *int
-	timestamp       time.Time
-	windDirection   *int
-	windGust        *int
-	windSpeed       *int
+// Zero zeroes all measurements in the observation payload.
+func (w *Wx) Zero() {
+	w.Timestamp = time.Time{}
+
+	w.Altimeter = 0
+	w.Humidity = -1
+	w.RainRate = -1.0
+	w.RainLast24Hours = -1.0
+	w.RainToday = -1.0
+	w.SolarRad = -1
+	w.Temp = -100
+	w.WindDir = -1
+	w.WindGust = 0
+	w.WindSpeed = -1
 }
 
-// Altimeter is the barometric pressure as altimeter
-// (station plus elevation correction) in inches.
-func (w *Wx) Altimeter(f float64) {
-	// Specification calls for millibars - convert in to mb.
-	v := f * 33.8637526
-	w.o.altimeter = &v
-}
-
-// Humidity is the relative humidity.
-func (w *Wx) Humidity(i int) {
-	// Specification states that 100 should be represented as 0.
-	v := i
-	if v == 100 {
-		v = 0
-	}
-	w.o.humidity = &v
-}
-
-// RainRate is the rain accumulation for the last hour
-// in inches.
-func (w *Wx) RainRate(f float64) {
-	w.o.rainRate = &f
-}
-
-// RainLast24Hours is the rain accumulation for the last 24 hours
-// in inches.
-func (w *Wx) RainLast24Hours(f float64) {
-	w.o.rainLast24Hours = &f
-}
-
-// RainToday is the rain accumulation so far today in inches.
-func (w *Wx) RainToday(f float64) {
-	w.o.rainToday = &f
-}
-
-// SolarRadiation is the solar radiation in W/m^2.
-func (w *Wx) SolarRadiation(i int) {
-	w.o.solarRadiation = &i
-}
-
-// Temperature is the temperature in F.
-func (w *Wx) Temperature(f float64) {
-	v := int(f)
-	w.o.temperature = &v
-}
-
-// Timestamp is the time of the observation.
-func (w *Wx) Timestamp(t time.Time) {
-	w.o.timestamp = t
-}
-
-// WindDirection is the wind direction in degrees.
-func (w *Wx) WindDirection(i int) {
-	w.o.windDirection = &i
-}
-
-// WindGust is the peak wind speed for the previous 5 minutes
-// in mph.
-func (w *Wx) WindGust(i int) {
-	w.o.windGust = &i
-}
-
-// WindSpeed is the wind speed in mph.
-func (w *Wx) WindSpeed(i int) {
-	w.o.windSpeed = &i
-}
-
-// Clear clears all measurements in the observation payload.
-func (w *Wx) Clear() {
-	w.o = obs{}
-}
-
-// llDecToDMS takes a float latitude or longitude and converts it to
+// decToDMS takes a float latitude or longitude and converts it to
 // degrees, minutes (second as decimal), and a hemisphere string.
-func llDecToDMS(ll float64, hems [2]string) (float64, float64, string) {
-	deg, frac := math.Modf(math.Abs(ll))
+func decToDMS(l float64, hems [2]string) (float64, float64, string) {
+	deg, frac := math.Modf(math.Abs(l))
 	min := frac * 60.0
-	hem := hems[0]
-	if ll < 0 {
-		hem = hems[1]
-	}
 
-	return deg, min, hem
+	if l < 0 {
+		return deg, min, hems[1]
+	}
+	return deg, min, hems[0]
 }
 
 // String returns an APRS packet for the provided measurements.
-func (w Wx) String() (t string) {
+func (w Wx) String() (s string) {
 	// Refer to APRS Protocol Reference 1.0
 	// Chapter 12: Weather Reports
 	//
 	// Parameters:
-	//   c = wind direction (in degrees) [4 bytes]
-	//   s = sustained one-minute wind speed (in mph) [4 bytes]
-	//   g = gust (peak wind speed in mph in the last 5 minutes) [4 bytes]
+	//   _ = wind direction (in degrees) [3 chars]
+	//   / = sustained one-minute wind speed (in mph) [3 chars]
+	//   g = gust (peak wind speed in mph in the last 5 minutes) [3 chars]
 	//   t = temperature (in degrees Fahrenheit). Temperatures below zero
-	//       are expressed as -01 to -99 [4 bytes]
-	//   r = rainfall (in hundredths of an inch) in the last hour [4 bytes]
-	//   p = rainfall (in hundredths of an inch) in the last 24 hours [4 bytes]
-	//   P = rainfall (in hundredths of an inch) since midnight [4 bytes]
-	//   h = humidity (in %. 00 = 100%) [3 bytes]
+	//       are expressed as -01 to -99 [3 chars]
+	//   r = rainfall (in hundredths of an inch) in the last hour [3 chars]
+	//   p = rainfall (in hundredths of an inch) in the last 24 hours [3 chars]
+	//   P = rainfall (in hundredths of an inch) since midnight [3 chars]
+	//   h = humidity (in %. 00 = 100%) [2 chars]
 	//   b = barometric altimeter pressure (in tenths of millibars/tenths of
-	//       hPascal) [5 bytes]
+	//       hPascal) [4 chars]
 	//
-	// Optional parameters:
-	//   L = luminosity (in watts per square meter) 999 and below [4 bytes]
-	//   l = luminosity (in watts per square meter) 1000 and above [4 bytes]
+	//   L = luminosity (in watts per square meter) 999 and below [3 chars]
+	//   l = luminosity (in watts per square meter) 1000 and above [3 chars]
 	//   (L is inserted in place of one of the rain values)
 	//   s = snowfall (in inches) in the last 24 hours
 	//   # = raw rain counter
 
 	// Timestamp as UTC two digit day, hour, and minute.
-	if w.o.timestamp.IsZero() {
-		w.o.timestamp = time.Now()
+	if w.Timestamp.IsZero() {
+		fmt.Println("doh!")
+		w.Timestamp = time.Now()
 	}
-	t += fmt.Sprintf("@%sz", w.o.timestamp.In(time.UTC).Format("021504"))
 
-	// Location
-	deg, min, hem := llDecToDMS(w.Lat, [2]string{"N", "S"})
-	t += fmt.Sprintf("%02.0f%02.2f%s", deg, min, hem)
-	deg, min, hem = llDecToDMS(w.Lon, [2]string{"E", "W"})
-	t += fmt.Sprintf("/%03.0f%02.2f%s", deg, min, hem)
+	// Base prefix
+	latDeg, latMin, latHem := decToDMS(w.Lat, [2]string{"N", "S"})
+	lonDeg, lonMin, lonHem := decToDMS(w.Lon, [2]string{"E", "W"})
+	s = fmt.Sprintf("@%sz%02.0f%02.2f%s/%03.0f%02.2f%s",
+		w.Timestamp.In(time.UTC).Format("021504"),
+		latDeg, latMin, latHem,
+		lonDeg, lonMin, lonHem)
 
 	// Parameters
-	if w.o.windDirection == nil {
-		t += "_..."
+	if w.WindDir < 0 {
+		s += "_..."
 	} else {
-		t += fmt.Sprintf("_%03d", *w.o.windDirection)
+		s += fmt.Sprintf("_%03d", w.WindDir)
 	}
 
-	if w.o.windSpeed == nil {
-		t += "/..."
+	if w.WindSpeed < 0 {
+		s += "/..."
 	} else {
-		t += fmt.Sprintf("/%03d", *w.o.windSpeed)
+		s += fmt.Sprintf("/%03d", w.WindSpeed)
 	}
 
-	if w.o.windGust == nil {
-		t += "g..."
+	if w.WindGust < 1 {
+		s += "g..."
 	} else {
-		t += fmt.Sprintf("g%03d", *w.o.windGust)
+		s += fmt.Sprintf("g%03d", w.WindGust)
 	}
 
-	if w.o.temperature == nil {
-		t += "t..."
+	if w.Temp < -99 {
+		s += "t..."
 	} else {
-		t += fmt.Sprintf("t%03d", *w.o.temperature)
+		s += fmt.Sprintf("t%03d", w.Temp)
 	}
 
-	if w.o.rainRate == nil {
-		t += "r..."
+	if w.RainRate < 0.0 {
+		s += "r..."
 	} else {
-		t += fmt.Sprintf("r%03.0f", *w.o.rainRate*100.0)
+		s += fmt.Sprintf("r%03.0f", w.RainRate*100.0)
 	}
 
-	if w.o.rainLast24Hours == nil {
-		t += "p..."
+	if w.RainLast24Hours < 0.0 {
+		s += "p..."
 	} else {
-		t += fmt.Sprintf("p%03.0f", *w.o.rainLast24Hours*100.0)
+		s += fmt.Sprintf("p%03.0f", w.RainLast24Hours*100.0)
 	}
 
-	if w.o.rainToday == nil {
-		t += "P..."
+	if w.RainToday < 0.0 {
+		s += "P..."
 	} else {
-		t += fmt.Sprintf("P%03.0f", *w.o.rainToday*100.0)
+		s += fmt.Sprintf("P%03.0f", w.RainToday*100.0)
 	}
 
-	if w.o.humidity == nil {
-		t += "h.."
+	if w.Humidity < 0 {
+		s += "h.."
 	} else {
-		t += fmt.Sprintf("h%02d", *w.o.humidity)
+		s += fmt.Sprintf("h%02d", w.Humidity%100)
 	}
 
-	if w.o.altimeter == nil {
-		t += "b....."
+	if w.Altimeter <= 0.0 {
+		s += "b....."
 	} else {
-		t += fmt.Sprintf("b%05.0f", *w.o.altimeter*10.0)
+		s += fmt.Sprintf("b%05.0f", w.Altimeter*33.8637526*10)
 	}
 
-	if w.o.solarRadiation != nil {
-		if *w.o.solarRadiation >= 1000 {
-			t += fmt.Sprintf("l%03d", *w.o.solarRadiation-1000)
-		} else {
-			t += fmt.Sprintf("L%03d", *w.o.solarRadiation)
-		}
+	if w.SolarRad >= 1000 {
+		s += fmt.Sprintf("l%03d", w.SolarRad-1000)
+	} else if w.SolarRad >= 0 {
+		s += fmt.Sprintf("L%03d", w.SolarRad)
 	}
 
-	// Software and weather unit type
-	t += fmt.Sprintf("%s_%s", SwName, SwVers)
+	// Software
+	s += SwName + SwVers
 	if w.Type != "" {
-		t += fmt.Sprintf("-%s", w.Type)
+		s += fmt.Sprintf("-%s", w.Type)
 	}
 
 	return
--- wx_test.go
+++ wx_test.go
@@ -11,69 +11,74 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func beginTest(w *Wx) {
-	w.Timestamp(time.Date(2016, time.November, 5, 20, 35, 0, 0, time.UTC))
-}
-
-func endTest(w *Wx) {
-	w.Clear()
-}
-
 func TestWxText(t *testing.T) {
-	const pre = "@052035z3542.00N/07842.00W_"
-	const suf = "GoTest_9.9-Stn"
-
 	a := assert.New(t)
 
-	SwName = "GoTest"
-	SwVers = "9.9"
 	w := Wx{
 		Lat:  35.7,
 		Lon:  -78.7,
 		Type: "Stn",
 	}
+	w.Zero()
+	w.Timestamp = time.Date(2016, time.November, 5, 20, 35, 0, 0, time.UTC)
 
-	beginTest(&w)
-	a.Equal(w.String(), pre+".../...g...t...r...p...P...h..b....."+suf)
-	w.WindDirection(180)
-	a.Equal(w.String(), pre+"180/...g...t...r...p...P...h..b....."+suf)
-	w.WindSpeed(8)
-	a.Equal(w.String(), pre+"180/008g...t...r...p...P...h..b....."+suf)
-	w.WindGust(16)
-	a.Equal(w.String(), pre+"180/008g016t...r...p...P...h..b....."+suf)
-	endTest(&w)
+	p := "@052035z3542.00N/07842.00W_"
+	s := SwName + SwVers + "-Stn"
 
-	beginTest(&w)
-	w.Temperature(72.0)
-	a.Equal(w.String(), pre+".../...g...t072r...p...P...h..b....."+suf)
-	endTest(&w)
+	// Zero state
+	a.Equal(p+".../...g...t...r...p...P...h..b....."+s, w.String())
 
-	beginTest(&w)
-	w.RainRate(0.54)
-	a.Equal(w.String(), pre+".../...g...t...r054p...P...h..b....."+suf)
-	w.RainLast24Hours(0.23)
-	a.Equal(w.String(), pre+".../...g...t...r054p023P...h..b....."+suf)
-	w.RainToday(0.21)
-	a.Equal(w.String(), pre+".../...g...t...r054p023P021h..b....."+suf)
-	endTest(&w)
+	// Custom software
+	SwName = "GoTst"
+	SwVers = "9"
+	s = "GoTst9-Stn"
+	a.Equal(p+".../...g...t...r...p...P...h..b....."+s, w.String())
 
-	beginTest(&w)
-	w.Humidity(61)
-	a.Equal(w.String(), pre+".../...g...t...r...p...P...h61b....."+suf)
-	w.Humidity(100)
-	a.Equal(w.String(), pre+".../...g...t...r...p...P...h00b....."+suf)
-	endTest(&w)
+	// Parameters
+	w.WindDir = 0
+	a.Equal(p+"000/...g...t...r...p...P...h..b....."+s, w.String())
+	w.WindDir = 180
+	a.Equal(p+"180/...g...t...r...p...P...h..b....."+s, w.String())
+	w.WindSpeed = 0
+	a.Equal(p+"180/000g...t...r...p...P...h..b....."+s, w.String())
+	w.WindSpeed = 8
+	a.Equal(p+"180/008g...t...r...p...P...h..b....."+s, w.String())
+	w.WindGust = 0
+	a.Equal(p+"180/008g...t...r...p...P...h..b....."+s, w.String())
+	w.WindGust = 16
+	a.Equal(p+"180/008g016t...r...p...P...h..b....."+s, w.String())
 
-	beginTest(&w)
-	w.Altimeter(29.87)
-	a.Equal(w.String(), pre+".../...g...t...r...p...P...h..b10115"+suf)
-	endTest(&w)
+	w.Temp = -20
+	a.Equal(p+"180/008g016t-20r...p...P...h..b....."+s, w.String())
+	w.Temp = 0
+	a.Equal(p+"180/008g016t000r...p...P...h..b....."+s, w.String())
+	w.Temp = 72
+	a.Equal(p+"180/008g016t072r...p...P...h..b....."+s, w.String())
 
-	beginTest(&w)
-	a.Equal(w.String(), pre+".../...g...t...r...p...P...h..b....."+suf)
-	w.SolarRadiation(864)
-	a.Equal(w.String(), pre+".../...g...t...r...p...P...h..b.....L864"+suf)
-	w.SolarRadiation(1864)
-	a.Equal(w.String(), pre+".../...g...t...r...p...P...h..b.....l864"+suf)
-	endTest(&w)
+	w.RainRate = 0.0
+	a.Equal(p+"180/008g016t072r000p...P...h..b....."+s, w.String())
+	w.RainRate = 0.54
+	a.Equal(p+"180/008g016t072r054p...P...h..b....."+s, w.String())
+	w.RainLast24Hours = 0
+	a.Equal(p+"180/008g016t072r054p000P...h..b....."+s, w.String())
+	w.RainLast24Hours = 0.23
+	a.Equal(p+"180/008g016t072r054p023P...h..b....."+s, w.String())
+	w.RainToday = 0.0
+	a.Equal(p+"180/008g016t072r054p023P000h..b....."+s, w.String())
+	w.RainToday = 0.21
+	a.Equal(p+"180/008g016t072r054p023P021h..b....."+s, w.String())
+
+	w.Humidity = 61
+	a.Equal(p+"180/008g016t072r054p023P021h61b....."+s, w.String())
+	w.Humidity = 100
+	a.Equal(p+"180/008g016t072r054p023P021h00b....."+s, w.String())
+
+	w.Altimeter = 29.87
+	a.Equal(p+"180/008g016t072r054p023P021h00b10115"+s, w.String())
+
+	a.Equal(p+"180/008g016t072r054p023P021h00b10115"+s, w.String())
+	w.SolarRad = 864
+	a.Equal(p+"180/008g016t072r054p023P021h00b10115L864"+s, w.String())
+	w.SolarRad = 1864
+	a.Equal(p+"180/008g016t072r054p023P021h00b10115l864"+s, w.String())
 }
