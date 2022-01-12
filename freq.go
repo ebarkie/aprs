0f89ef33b19be3067be8f192514dda00a6425a6d
Dave Josephsen <djosephsen@fastly.com>
2022-01-12 10:56:00 -0600

    remove copyright

--- freq.go
+++ freq.go
@@ -1,6 +1,3 @@
-// Copyright (c) 2016 Eric Barkie. All rights reserved.  // Use of this source code is governed by the MIT license
-// that can be found in the LICENSE file.
-
 package aprs
 
 import (
