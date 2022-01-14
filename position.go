21b8d66d021aa348127cf5db1a605bc82c99d002
Dave Josephsen <djosephsen@fastly.com>
2022-01-13 20:46:09 -0600

    refactor position.go to use string concatenation

--- position.go
+++ position.go
@@ -11,52 +11,47 @@ type PositionReport struct {
 	Lat            float64   // latitude
 	Lon            float64   // longitude
 	Altitude       int
-	Symbol         []byte // 2 byte Map symbol; see Chapter 20 aprs101
-	Extn           []byte // 7+ byte Data Extension field. See Chapter 7 pg27 aprs101
-	Freq           *Freq  // freqspec compatible Frequency report
-	Comment        []byte // free-form comment
-	MessageCapable bool   // Stations without APRS messaging capability are typically stand-alone trackers or digipeaters.
+	Symbol         []string // 2 byte Map symbol; see Chapter 20 aprs101
+	Extn           string   // 7+ byte Data Extension field. See Chapter 7 pg27 aprs101
+	Freq           *Freq    // freqspec compatible Frequency report
+	Comment        string   // free-form comment
+	MessageCapable bool     // Stations without APRS messaging capability are typically stand-alone trackers or digipeaters.
 }
 
-// String wraps p.Bytes() to return a rendered position report string
+// String returns a rendered position report suitable for sending to a TNC
 func (p *PositionReport) String() string {
-	return string(p.Bytes())
-}
-
-// Bytes returns a rendered position report suitable for sending to a TNC
-func (p *PositionReport) Bytes() []byte {
 	// Refer to APRS protocol reference 1.0
 	// Chapter 8: position and df report data formats
-	out := []byte{}
+	var out string
 
 	// render the data type
-	out = append(out, p.renderDataType())
+	out = string(p.renderDataType())
 
 	// render the timestamp
 	if !p.Timestamp.IsZero() {
-		out = append(out, p.renderTimestamp()...)
+		out = out + p.renderTimestamp()
 	}
 
 	// render the lat/long coords
-	out = append(out, p.renderCoords()...)
+	out = out + p.renderCoords()
 
 	// render the data extension block (must be at least 7 bytes)
 	if len(p.Extn) >= 7 {
-		out = append(out, p.Extn...)
+		out = out + p.Extn
 	}
 
 	// render the Freq if it exists
 	if p.Freq != nil {
-		out = append(out, []byte(p.renderFreq())...)
+		out = out + p.renderFreq()
 	}
 
 	// render altitude if it exists
 	if p.Altitude > 0 {
-		out = append(out, p.renderAltitude()...)
+		out = out + p.renderAltitude()
 	}
 
 	// add any other comments
-	out = append(out, p.Comment...)
+	out = out + p.Comment
 
 	// truncate to the size of the ui frame and return
 	return out[:255]
@@ -77,35 +72,35 @@ func (p *PositionReport) renderDataType() byte {
 }
 
 // renderTimestamp returns the rendered timestamp from the position report
-func (p *PositionReport) renderTimestamp() []byte {
-	return []byte(fmt.Sprintf("%sz", p.Timestamp.Format("021504")))
+func (p *PositionReport) renderTimestamp() string {
+	return fmt.Sprintf("%sz", p.Timestamp.Format("021504"))
 }
 
 // renderCoords returns the rendered latitude and longitude from the position report
-func (p *PositionReport) renderCoords() []byte {
+func (p *PositionReport) renderCoords() string {
 	latDeg, latMin, latHem := decToDMS(p.Lat, [2]string{"N", "S"})
 	lonDeg, lonMin, lonHem := decToDMS(p.Lon, [2]string{"E", "W"})
 
-	return []byte(fmt.Sprintf("%02.0f%05.2f%s%s%03.0f%05.2f%s%s",
+	return fmt.Sprintf("%02.0f%05.2f%s%s%03.0f%05.2f%s%s",
 		latDeg, latMin, latHem,
-		string(p.Symbol[0]),
+		p.Symbol[0],
 		lonDeg, lonMin, lonHem,
-		string(p.Symbol[1])))
+		p.Symbol[1])
 }
 
 // renderFreq returns the rendered freqspec compatible Frequency
-func (p *PositionReport) renderFreq() []byte {
-	out := []byte{}
+func (p *PositionReport) renderFreq() string {
+	var out string
 	// add a delimiter if a data-extension exists
 	if len(p.Extn) >= 7 {
-		out = append(out, '/')
+		out = out + `/`
 	}
-	return append(out, p.Freq.Render()...)
+	return (out + p.Freq.Render())
 }
 
 // renderAltitude returns the rendered freqspec compatible Frequency
-func (p *PositionReport) renderAltitude() []byte {
-	return []byte(fmt.Sprintf("/A=%06d", p.Altitude))
+func (p *PositionReport) renderAltitude() string {
+	return fmt.Sprintf("/A=%06d", p.Altitude)
 }
 
 // Data Extensions:
@@ -125,23 +120,23 @@ func (p *PositionReport) CSExtension(course, speed, bearing, nrq int) {
 	if bearing+nrq > 0 {
 		// if Direction-finding data is included, hard-code the map-symbol to DF
 		// aprs 101, pg 34; df reports
-		p.Symbol = []byte(`/\`)
-		p.Extn = []byte(fmt.Sprintf("%s/%s/%s/%s",
+		p.Symbol = []string{`/`, `\`}
+		p.Extn = fmt.Sprintf("%s/%s/%s/%s",
 			z3p(course),
 			z3p(speed),
 			z3p(bearing),
-			z3p(nrq)))
+			z3p(nrq))
 	}
-	p.Extn = []byte(fmt.Sprintf("%s/%s",
+	p.Extn = fmt.Sprintf("%s/%s",
 		z3p(course),
-		z3p(speed)))
+		z3p(speed))
 }
 
 // DSExtension Extension sets a wind direction/speed data-extension block
 func (p *PositionReport) DSExtension(direction, speed int) {
-	p.Extn = []byte(fmt.Sprintf("%s/%s",
+	p.Extn = fmt.Sprintf("%s/%s",
 		z3p(direction),
-		z3p(speed)))
+		z3p(speed))
 }
 
 // PHGExtension sets a power/height/gain data-extension block
@@ -154,16 +149,16 @@ func (p *PositionReport) PHGExtension(power, gain, dir int, height byte) {
 	//| Gain        | 0    | 1    | 2   | 3     | 4    | 5     | 6    | 7     | 8    |    9 | dB    |
 	//| Directivity | omni | 45NE | 90E | 135SE | 180S | 225SW | 270W | 315NW | 360N |      | de    |
 	//+-------------+------+------+-----+-------+------+-------+------+-------+------+------+-------+
-	p.Extn = []byte(fmt.Sprintf("PHG%d%s%d%d",
+	p.Extn = fmt.Sprintf("PHG%d%s%d%d",
 		max(9, power),
 		string(height), // `The height code may in fact be any ASCII character 0–9 and above`
 		max(9, gain),
-		max(8, dir)))
+		max(8, dir))
 }
 
 // RNGExtension sets a pre-computed range data-extension block
 func (p *PositionReport) RNGExtension(miles int) {
-	p.Extn = []byte(fmt.Sprintf("RNG%04d", miles))
+	p.Extn = fmt.Sprintf("RNG%04d", miles)
 }
 
 // DFSExtension sets a Direction-finding data-extension block
@@ -176,10 +171,10 @@ func (p *PositionReport) DFSExtension(str, gain, dir int, height byte) {
 	//| Gain        | 0    | 1    | 2   | 3     | 4    | 5     | 6    | 7     | 8    | 9    | dB       |
 	//| Directivity | omni | 45NE | 90E | 135SE | 180S | 225SW | 270W | 315NW | 360N |      | deg      |
 	//+-------------+------+------+-----+-------+------+-------+------+-------+------+------+----------+
-	p.Symbol = []byte(`/\`)
-	p.Extn = []byte(fmt.Sprintf("DFS%d%s%d%d",
+	p.Symbol = []string{`/`, `\`}
+	p.Extn = fmt.Sprintf("DFS%d%s%d%d",
 		max(9, str),
 		string(height),
 		max(9, gain),
-		max(8, dir)))
+		max(8, dir))
 }
